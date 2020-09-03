147b01928fc992b50eefc3396dffa1bc02a38876
Ivan Vilanculo <isneezy@users.noreply.github.com>
2020-09-03 16:27:57 +0200

    add register dependencies and create sample page

---
+++ examples/nuxjs/nuxt.config.js
@@ -0,0 +1,13 @@
+export default {
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css"
+      }
+    ]
+  },
+  plugins: ["~/plugins/vue-selectize"]
+};
---
+++ examples/nuxjs/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="margin-top: 10px">
+    <v-selectize v-model="value" :options="['Example', 'Vue-selectize', 'Nuxtjs']" multiple/>
+    <p style="margin-top: 8px">You have selected: {{ valueAsTring }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    value: ["Vue-selectize"]
+  }),
+  computed: {
+    valueAsTring() {
+      return this.value.join(", ");
+    }
+  }
+};
+</script>
